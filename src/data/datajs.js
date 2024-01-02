7b4ac84f1fe073afde6b06f04d5dc0aec6925069
Cristian Contreras <contreras.cristian994@gmail.com>
2024-01-02 06:30:07 -0500

    [data]: Add data

---
+++ src/data/datajs.js
@@ -0,0 +1,139 @@
+   import React from "react";
+   
+   export const food = [
+        {
+            "id": 1,
+            "name": "Hotdog & Soda",
+            "price": 1.50,
+            "image": "https://www.eatthis.com/wp-content/uploads/sites/4/2022/04/costco-hot-dog-soda.jpg?quality=82&strip=1"
+        },
+        {
+            "id": 2,
+            "name": "Slice Pepperoni",
+            "price": 1.99,
+            "image": "https://img.freepik.com/premium-photo/slice-pizza-wooden-background-top-view_128937-610.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Slice Cheese",
+            "price": 1.99,
+            "image": "https://images.squarespace-cdn.com/content/v1/5c96e9a0755be272d50dc819/1557033275310-EVUULNX7KDZYK895OCPX/1_Ben%27s-Pizza_0016.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Rostisserie Chicken Caesar Salad",
+            "price": 6.99,
+            "image": "https://asset-delivery.epicure.com/4f365e04-333c-00ab-a1e0-d5cc0e128aa9/2516e305-6716-42da-ab6c-e0242add3aae/rotisserie_chicken_caesar_salad_1036x583.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Chicken Bake",
+            "price": 3.99,
+            "image": "https://www.tastingtable.com/img/gallery/caesar-dressing-is-secret-ingredient-in-costcos-beloved-chicken-bakes/intro-1699990272.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Roast Beef Sandwich",
+            "price": 9.99,
+            "image": "https://www.eatthis.com/wp-content/uploads/sites/4/2019/01/low-calorie-roast-beef-cheddar-sandwiches-horseradish-mayo.jpg?quality=82&strip=1"
+        },
+        {
+            "id": 7,
+            "name": "Twisted Churro Cinnamon & Sugar",
+            "price": 1.49,
+            "image": "https://www.aspicyperspective.com/wp-content/uploads/2022/06/Easy-Cinnamon-Twists-16.jpg"
+        },
+        {
+            "id": 8,
+            "name": "20 oz Soda with Refill",
+            "price": 0.69,
+            "image": "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_150,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_130/wor0ubpptnx8vyuhfhot"
+        }
+    ];
+
+    export const pizzaAndBrew = [
+        {
+            "id": 1,
+            "name": "Whole Pepperoni",
+            "image": "https://i0.wp.com/awesomepizza.ca/wp-content/uploads/2015/01/Papperoni.jpg?fit=700%2C678&ssl=1",
+            "price": 9.95
+        },
+        {
+            "id": 2,
+            "name": "Whole Cheese",
+            "image": "https://143892534.cdn6.editmysite.com/uploads/1/4/3/8/143892534/s332370591201761310_p201_i2_w3000.jpeg?width=2560",
+            "price": 9.95
+        },
+        {
+            "id": 3,
+            "name": "1/2 Pepperoni - 1/2 Cheese",
+            "image": "https://d2j6dbq0eux0bg.cloudfront.net/images/26721109/1363123115.jpg",
+            "price": 9.95
+        },
+        {
+            "id": 4,
+            "name": "Cold Brew Mocha Freeze",
+            "image": "https://majestycoffee.com/cdn/shop/articles/Latte_Freeze.jpg?v=1684145563",
+            "price": 2.99
+        },
+        {
+            "id": 5,
+            "name": "Fruit Smoothie",
+            "image": "https://media.istockphoto.com/id/1313644815/photo/strawberry-smoothie.jpg?s=612x612&w=0&k=20&c=CJ5DBRKWWX_NrH19EIRgJny7gPGNxF8gIVpHNFk8OMs=",
+            "price": 2.99
+        },
+        {
+            "id": 6,
+            "name": "Cold Brew Latte Freeze",
+            "image": "https://i5.walmartimages.com/seo/100-Pack-32-oz-Clear-Plastic-Cups-Flat-Lids-Disposable-Iced-Coffee-Cups-BPA-Free-Premium-Crystal-Smoothie-Cup-Party-Lemonade-Stand-Cold-Drinks-Juice_e8d99713-aa9a-452f-b3d5-21c5db314564.d18886586d9b1b03e8f25c5c35934569.jpeg",
+            "price": 2.99
+        }
+    ];
+
+    export const StrawberrySundae = [
+        {
+            "id": 1,
+            "name": "Strawberry Ice Cream Sundae",
+            "image": "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F2unw8xss01eb1.jpg",
+            "price": 2.99
+        },
+        {
+            "id": 2,
+            "name": "Strawberry Ice Cream Cup",
+            "image": "https://www.iheartnaptime.net/wp-content/uploads/2022/05/I-Heart-Naptime-strawberry-dole-whip-5.jpg",
+            "price": 1.99
+        },
+        {
+            "id": 3,
+            "name": "Strawberry / Vanilla Twist Sundae",
+            "image": "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F2unw8xss01eb1.jpg",
+            "price": 2.99
+        },
+        {
+            "id": 4,
+            "name": "Strawberry / Vanilla Twist Ice Cream",
+            "image": "https://www.iheartnaptime.net/wp-content/uploads/2022/05/I-Heart-Naptime-strawberry-dole-whip-5.jpg",
+            "price": 1.99
+        }
+    ];
+
+    export const VanillaSundae = [
+        {
+            "id": 1,
+            "name": "Vanilla Ice Cream / Strawberry Sundae",
+            "image": "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F2unw8xss01eb1.jpg",
+            "price": 2.49
+        },
+        {
+            "id": 2,
+            "name": "Vanilla Ice Cream Cup",
+            "image": "https://www.webstaurantstore.com/images/products/extra_large/602416/2415970.jpg",
+            "price": 1.99
+        },
+        {
+            "id": 3,
+            "name": "Vanilla Ice Cream / Chocolate Sundae",
+            "image": "https://i.pinimg.com/originals/36/d1/05/36d10522eb05a602f3cc14d71817e989.jpg",
+            "price": 2.49
+        }
+    ];
