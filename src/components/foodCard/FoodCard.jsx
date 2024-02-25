192cd94148cc191e1bc8f54362353b6cbd96b98f
Cristian Contreras <contreras.cristian994@gmail.com>
2024-02-25 11:23:21 -0500

    [css]: grid updated

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import '../foodCard/FoodCard.css';
 
 function FoodCard({ foodItem }) {
+    const {id, image, name, price} = foodItem;
   return (
-      <div className='foodCard' key={foodItem.id}>
-          <img src={foodItem.image} alt={foodItem.name} />
+      <div className='foodCard' key={id}>
+          <img src={image} alt={name} />
           <div className='info-food-card'>
               <div className='name-section'>
-                  <p className='item-name'>{foodItem.name}</p>
+                  <p className='item-name'>{name}</p>
               </div>
               <div className='price-section'>
-                  <p className='item-price'>${foodItem.price}</p>
+                  <p className='item-price'>${price}</p>
               </div>
           </div>
       </div>
