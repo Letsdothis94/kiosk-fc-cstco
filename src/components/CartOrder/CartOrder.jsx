861dcc1e296cf0289a958ffcd6e0670fe1fb6cbc
Cristian Contreras <contreras.cristian994@gmail.com>
2024-02-27 03:22:54 -0500

    [cartOrder]: Created component, added css

---
+++ src/components/CartOrder/CartOrder.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import SelectedItem from '../selectedItem/SelectedItem';
+import '../CartOrder/CartOrder.css';
+
+const CartOrder = () => {
+  return (
+      <section className='order-view-screen'>
+          <div className='title-view'>
+              <p>You can change or remove an item on the receipt by tapping it.</p>
+          </div>
+          <div className='order-items-selected'>
+              <SelectedItem />
+          </div>
+          <div className='sub-total-div'>
+              <p>Sub-Total:</p>
+              <p>$0.00</p>
+          </div>
+          <div className='btn-cancel'>
+              <p>Cancel Order</p>
+          </div>
+          <div className='btn-pay'>
+              <p>Pay for Order</p>
+          </div>
+      </section>
+  )
+}
+
+export default CartOrder;
